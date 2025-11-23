c831890dd4676400fda643d8fdaf9be1d94edabb
ShelD1m <1254832@edu.rut-miit.ru>
2025-11-24 01:56:40 +0300

    Shelukhin: заменил solver

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,27 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int dif(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+
+    public int div(int a, int b) {
+        if (b != 0) {
+            return a / b;
+        }
+        return 0;
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver() {
+        int x = add(8, 8);
+        int y = dif(20, 16);
+        int z = div(x, y);
+        return z;
     }
 }
